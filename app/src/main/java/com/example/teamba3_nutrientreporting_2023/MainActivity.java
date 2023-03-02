fa55820139bd9d2b328441342e8f7ca50b856bca
Astoria <netadmin@broken-moon.net>
2023-03-02 15:19:03 -0600

    Add default settings screen

--- app/src/main/java/com/example/teamba3_nutrientreporting_2023/MainActivity.java
+++ app/src/main/java/com/example/teamba3_nutrientreporting_2023/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.teamba3_nutrientreporting_2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToSettings(View view) {
+        Intent itent = new Intent(MainActivity.this, SettingsActivity.class);
+        MainActivity.this.startActivity(itent);
+    }
+
 }
