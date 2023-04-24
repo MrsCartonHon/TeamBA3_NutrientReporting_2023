5aa3956e47adaf8cffab06e8b1cc2e7a219a793b
Astoria <netadmin@broken-moon.net>
2023-04-24 15:18:28 -0500

    Cleanup all the various classes

--- app/src/main/java/com/example/teamba3_nutrientreporting_2023/CalcActivity.java
+++ app/src/main/java/com/example/teamba3_nutrientreporting_2023/CalcActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class CalcActivty extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class CalcActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +20,12 @@ public class CalcActivty extends AppCompatActivity implements AdapterView.OnItem
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dummy_fertilizers, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-
     }
     public void equals(View view){
-        System.out.println("Doin M A F F");
         EditText test = (EditText)(findViewById(R.id.amountOfFertilizer));
         double amnt = Double.parseDouble(test.getText().toString())/0.18;
         TextView output = (TextView)(findViewById(R.id.output));
-        output.setText(amnt +" pounds per acre");
+        output.setText(amnt + getString(R.string.calcPoundArea));
     }
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
--- app/src/main/java/com/example/teamba3_nutrientreporting_2023/LoginActivity.java
+++ app/src/main/java/com/example/teamba3_nutrientreporting_2023/LoginActivity.java
@@ -8,21 +8,14 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Button;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.AuthResult;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailTextView, passwordTextView;
-    private Button Btn;
     private ProgressBar progressbar;
 
     private FirebaseAuth mAuth;
@@ -37,17 +30,11 @@ public class LoginActivity extends AppCompatActivity {
         // initialising all views through id defined above
         emailTextView = findViewById(R.id.email);
         passwordTextView = findViewById(R.id.password);
-        Btn = findViewById(R.id.login);
+        Button btn = findViewById(R.id.login);
         progressbar = findViewById(R.id.progressBar);
 
         // Set on Click Listener on Sign-in button
-        Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                loginUserAccount();
-            }
-        });
+        btn.setOnClickListener(view -> loginUserAccount());
     }
 
     private void loginUserAccount()
@@ -63,58 +50,53 @@ public class LoginActivity extends AppCompatActivity {
 
         // validations for input email and password
         if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(),
-                            "Please enter email!!",
-                            Toast.LENGTH_LONG)
-                    .show();
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Please enter email!!",
+                    Toast.LENGTH_LONG
+            ).show();
             return;
         }
 
         if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(),
-                            "Please enter password!!",
-                            Toast.LENGTH_LONG)
-                    .show();
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Please enter password!!",
+                    Toast.LENGTH_LONG
+            ).show();
             return;
         }
 
         // signin existing user
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                        new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(
-                                    @NonNull Task<AuthResult> task)
-                            {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(),
-                                                    "Login successful!!",
-                                                    Toast.LENGTH_LONG)
-                                            .show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Login successful!!",
+                            Toast.LENGTH_LONG
+                    ).show();
 
-                                    // hide the progress bar
-                                    //progressBar.setVisibility(View.GONE);
+                    // hide the progress bar
+                    //progressBar.setVisibility(View.GONE);
 
-                                    // if sign-in is successful
-                                    // intent to home activity
-                                    Intent intent
-                                            = new Intent(LoginActivity.this,
-                                            MainActivity.class);
-                                    startActivity(intent);
-                                }
+                    // if sign-in is successful
+                    // intent to home activity
+                    Intent intent = new Intent(
+                            LoginActivity.this,
+                            MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // sign-in failed
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Login failed!!",
+                            Toast.LENGTH_LONG
+                    ).show();
 
-                                else {
-
-                                    // sign-in failed
-                                    Toast.makeText(getApplicationContext(),
-                                                    "Login failed!!",
-                                                    Toast.LENGTH_LONG)
-                                            .show();
-
-                                    // hide the progress bar
-                                    progressbar.setVisibility(View.GONE);
-                                }
-                            }
-                        });
+                    // hide the progress bar
+                    progressbar.setVisibility(View.GONE);
+                }
+            }
+        );
     }
 }
